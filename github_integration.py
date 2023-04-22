41049dc1041b4025f4421f14817c825b1b87041c
casualshammy <axio@axio.name>
2023-04-22 17:52:46 +0100

    up(github_integrations): logging

--- github_integration.py
+++ github_integration.py
@@ -10,3 +10,4 @@ github_env = os.getenv('GITHUB_ENV')
 if (github_env != None):
     with open(github_env, "a") as github_env_file:
         github_env_file.write(f"tag={version}")
+        print(f"Set env var to github: tag={version}", flush= True)
