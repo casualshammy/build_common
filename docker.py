b668a0e15cbcced0f05ac4c0c2b5d048eda2e13b
casualshammy <axio@axio.name>
2024-06-19 16:48:25 +0100

    feature(docker): added `buildPush` method

---
+++ docker.py
@@ -0,0 +1,14 @@
+from .utils import callThrowIfError
+
+def buildPush(
+    _tag: str, 
+    _dockerfile: str,
+    _login: str, 
+    _pass: str, 
+    _shell: bool = False) -> None:
+  callThrowIfError(f"docker login -u {_login} -p {_pass}", _shell)
+  try:
+    callThrowIfError(f"docker build --tag {_tag} -f {_dockerfile} .", _shell)
+    callThrowIfError(f"docker push {_tag}", _shell)
+  finally:
+    callThrowIfError(f"docker logout", _shell)
