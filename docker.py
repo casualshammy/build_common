3b350459dd46137b18a3c565b8c3171fa5dcd6da
casualshammy <axio@axio.name>
2025-10-18 00:32:12 +0300

    feature(docker): added method 'buildPushMultiArch'

--- docker.py
+++ docker.py
@@ -12,3 +12,16 @@ def buildPush(
     utils.callThrowIfError(f"docker push {_tag}", _shell)
   finally:
     utils.callThrowIfError(f"docker logout", _shell)
+
+def buildPushMultiArch(
+    _tag: str, 
+    _dockerfile: str,
+    _login: str, 
+    _pass: str, 
+    _shell: bool = True) -> None:
+  utils.callThrowIfError(f"docker login -u {_login} -p {_pass}", _shell)
+  try:
+    # Build and push multi-architecture image using buildx
+    utils.callThrowIfError(f"docker buildx build --platform linux/amd64,linux/arm64 --tag {_tag} -f {_dockerfile} --push .", _shell)
+  finally:
+    utils.callThrowIfError(f"docker logout", _shell)
